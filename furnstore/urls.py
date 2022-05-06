508169ca440227e04fdf07e7e30804593644f5c6
Guldana1 <91088476+Guldana1@users.noreply.github.com>
2022-05-06 15:01:55 +0600

    Update urls.py

--- furnstore/urls.py
+++ furnstore/urls.py
@@ -1,16 +1,26 @@
+from django.conf import settings
+from multiprocessing.dummy import Namespace
+from xml.etree.ElementInclude import include
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
-from django.contrib.auth import views as auth_view
+from django.conf.urls.static import static
 
 urlpatterns = [
         #Leave as empty string for base url
-	path('', views.store, name="store"),
+	path('', views.home, name="home"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
 	path('register/', views.register, name='register'),
-    path('login/', auth_view.LoginView.as_view(template_name='store/login.html'), name="login"),
+    path('signin/', views.signin, name = 'login'),
+	path('signout/', views.signout, name = 'logout'),
     path('profile/', views.profile, name='profile'),
 	path('update_item/', views.updateItem, name="update_item"),
 	path('process_order/', views.processOrder, name="process_order"),
+    path('product/',views.product,name='product'),
 
 ]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
